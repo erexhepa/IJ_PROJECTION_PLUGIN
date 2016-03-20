4bc68dc71192b258bda7ed482f701702b5d5bf79
erexhepa <elton.rexhepaj@gmail.com>
2016-03-20 15:44:35 +0100

    Draft with the enerty optimisation step coded with no errors. No test done to check with Matlab version.

--- IJ2/ij/plugin/filter/SME_PROJECTION_SRC/SME_ENS_EnergyOptimisation.java
+++ IJ2/ij/plugin/filter/SME_PROJECTION_SRC/SME_ENS_EnergyOptimisation.java
@@ -19,10 +19,10 @@ public class SME_ENS_EnergyOptimisation {
     private SME_Plugin sme_plugin   = null;
     private final int KMEAN_NORM    = 3;
     private final double ENERGY_STEP= 0.0001;
-    private double[][] kmeanOutput ;
+    private RealMatrix kmeanOutput ;
     private RealMatrix rawdata2D    = null;
     private RealMatrix tmpProcess   = null;
-    private double optStep ;
+    private double step ;
 
     private int stepNumber          = 100;
     private RealMatrix idmax        = null;
@@ -32,172 +32,111 @@ public class SME_ENS_EnergyOptimisation {
     private RealMatrix movmat       = null;
     private RealMatrix mink         = null;
     private int iter                = 2;
-
+    private RealMatrix edgeflag     = null;
+    private RealMatrix edgeflag2    = null;
 
     public SME_ENS_EnergyOptimisation(SME_Plugin refplugin){
         sme_plugin = refplugin;
     }
 
-    public double[][] getMaxProjectionIndex(ImageStack imageStack){
 
-        double[][] maxIndex = new double[imageStack.getProcessor(1).getWidth()]
-                [imageStack.getProcessor(1).getHeight()];
-
-        for(int pixIndexI=0;pixIndexI<=imageStack.getProcessor(1).getWidth();pixIndexI++){
-            for(int pixIndexJ=0;pixIndexJ<=imageStack.getProcessor(1).getWidth();pixIndexJ++){
-                double maxZval = 0;
-
-                for(int pixIndexZ=0;pixIndexZ<=imageStack.getSize();pixIndexZ++){
-                    if(maxZval>imageStack.getVoxel(pixIndexI,pixIndexJ,pixIndexZ)){
-                        maxIndex[pixIndexI][pixIndexJ] = pixIndexZ;
-                    }
-                }
-            }
-        }
-
-        return(maxIndex);
-    }
 
     public void initOptimisation(){
-        RealMatrix edgeflag = MatrixUtils.createRealMatrix(sme_plugin.getKmeanCentroids());
-        RealMatrix edgeflag2  = edgeflag.scalarAdd(-1).scalarMultiply(1/KMEAN_NORM);
-        kmeanOutput = getMaxProjectionIndex(sme_plugin.getStack1());
+        edgeflag = MatrixUtils.createRealMatrix(sme_plugin.getKmeanCentroids());
+        edgeflag2  = edgeflag.scalarAdd(-1).scalarMultiply(1/KMEAN_NORM);
+        kmeanOutput = SME_ENS_Utils.getMaxProjectionIndex(sme_plugin.getStack1());
 
-        idmax       = MatrixUtils.createRealMatrix(kmeanOutput);
+        idmax       = kmeanOutput.copy();
         idmaxk      = idmax.copy();
         idmaxki     = idmax.copy();
         mink        = idmax.copy().scalarMultiply(0).scalarAdd(1);
 
-        optStep     = sme_plugin.getStack1().getSize()/(double)stepNumber;
+        step     = sme_plugin.getStack1().getSize()/(double)stepNumber;
         cost     = MatrixUtils.createRealMatrix(new double[1][stepNumber]);
         cost.setEntry(0,1,100);cost.setEntry(0,2,10);
     }
 
-    public void applyEnergyOptimisation(){
+    public void applyEnergyOptimisation() {
         RealMatrix idmax1 = idmaxk.copy();
         RealMatrix idmax2 = idmaxk.copy();
-        RealMatrix idmaxkB,IB = null;
+        RealMatrix idmaxkB, IB = null;
         ZProjector zproject = new ZProjector();
         zproject.setMethod(0);
 
-        while(Math.abs(cost.getEntry(0,iter)-cost.getEntry(0,(iter-1)))>(ENERGY_STEP)){
+        while (Math.abs(cost.getEntry(0, iter) - cost.getEntry(0, (iter - 1))) > (ENERGY_STEP)) {
             iter++;
             idmax1 = idmaxk.scalarAdd(ENERGY_STEP).copy();
             idmax2 = idmaxk.scalarAdd(-ENERGY_STEP).copy();
 
-            idmaxkB = padSymetricMatrix(idmaxk,true);
-            IB      = padSymetricMatrix(idmaxkB,true);
+            idmaxkB = SME_ENS_Utils.padSymetricMatrix(idmaxk, true);
+            IB = SME_ENS_Utils.padSymetricMatrix(idmaxkB, true);
 
-            ImageStack base    =    find_base(IB,3);
-            zproject.setImage(new ImagePlus("IterativeProjection",base));
+            ImageStack base = SME_ENS_Utils.find_base(IB, 3);
+            zproject.setImage(new ImagePlus("IterativeProjection", base));
             zproject.doProjection();
-            RealMatrix Mold    =    MatrixUtils.createRealMatrix(
+            RealMatrix Mold = MatrixUtils.createRealMatrix(
                     SME_ENS_Utils.convertFloatMatrixToDoubles(
                             zproject.getProjection().getImageStack().getProcessor(1).getFloatArray(),
-                    idmax1.getRowDimension(),idmax1.getColumnDimension()));
+                            idmax1.getRowDimension(), idmax1.getColumnDimension()));
+
+            ImageStack varoldStack = SME_ENS_Utils.repmatMatrixVar(Mold, base);
+
+            zproject.setImage(new ImagePlus("IterativeProjection", varoldStack));
+            zproject.setMethod(3);
+            zproject.doProjection();
+            RealMatrix varold2 = MatrixUtils.createRealMatrix(
+                    SME_ENS_Utils.convertFloatMatrixToDoubles(
+                            zproject.getProjection().getImageStack().getProcessor(1).getFloatArray(),
+                            idmax1.getRowDimension(), idmax1.getColumnDimension()));
+
+            RealMatrix d1 = idmax.subtract(idmax1).multiply(edgeflag2);
+            RealMatrix d2 = idmax.subtract(idmax2).multiply(edgeflag2);
+            RealMatrix d0 = idmax.subtract(idmaxk).multiply(edgeflag2);
+
+            RealMatrix M11 = idmax1.subtract(Mold);
+            RealMatrix M12 = idmax2.subtract(Mold);
+            RealMatrix M10 = idmaxk.subtract(Mold);
+
+            RealMatrix s1 = SME_ENS_Utils.realmatrixDoublepow(varold2.add(M11.multiply(
+                    idmax1.subtract(Mold.add(M11.scalarMultiply(1 / (double) 9))))).scalarMultiply(1 / 8), 0.5).scalarMultiply(9);
+            RealMatrix s2 = SME_ENS_Utils.realmatrixDoublepow(varold2.add(M11.multiply(
+                    idmax2.subtract(Mold.add(M12.scalarMultiply(1 / (double) 9))))).scalarMultiply(1 / 8), 0.5).scalarMultiply(9);
+            RealMatrix s0 = SME_ENS_Utils.realmatrixDoublepow(varold2.add(M11.multiply(
+                    idmaxk.subtract(Mold.add(M10.scalarMultiply(1 / (double) 9))))).scalarMultiply(1 / 8), 0.5).scalarMultiply(9);
+
+            RealMatrix c1 = d1.add(s1);
+            RealMatrix c2 = d2.add(s2);
+            RealMatrix c0 = d0.add(s0);
+
+            ImageStack catStack = new ImageStack(c0.getRowDimension(), c0.getColumnDimension());
+            catStack = SME_ENS_Utils.realmatrixCat(
+                    SME_ENS_Utils.realmatrixCat(
+                            SME_ENS_Utils.realmatrixCat(catStack,
+                                    c0), c1), c2);
+
+            zproject.setImage(new ImagePlus("IterativeProjection", catStack));
+            zproject.setMethod(2);
+            zproject.doProjection();
+            RealMatrix minc = MatrixUtils.createRealMatrix(
+                    SME_ENS_Utils.convertFloatMatrixToDoubles(
+                            zproject.getProjection().getImageStack().getProcessor(1).getFloatArray(),
+                            idmax1.getRowDimension(), idmax1.getColumnDimension()));
+
+            RealMatrix shiftc   =   SME_ENS_Utils.getMinProjectionIndex(catStack);
+            shiftc              =   shiftc.scalarAdd(-1);
+            SME_ENS_Utils.replaceRealmatElements(shiftc,1,step);
+            SME_ENS_Utils.replaceRealmatElements(shiftc,2,-step);
+
+            idmaxk  =   idmaxk.add(shiftc);
+            RealVector costIter = SME_ENS_Utils.realmat2vector(minc,0);
+            double costIterStep = costIter.getL1Norm()/(minc.getRowDimension()*minc.getColumnDimension());
+            cost.setEntry(0,iter,costIterStep);
+
+            step=step*0.99;
+
+            System.out.println(Integer.toString(iter));
+            System.out.println(Double.toString(costIterStep));
         }
-
-        /**
-         varold2=sum((base-repmat(Mold,[1 1 8])).^2,3);
-
-         d1=abs(idmax-idmax1).*edgeflag2;
-         d2=abs(idmax-idmax2).*edgeflag2;
-         d0=abs(idmax-idmaxk).*edgeflag2;
-
-         M11=idmax1-Mold;
-         M12=idmax2-Mold;
-         M10=idmaxk-Mold;
-         */
     }
 
-    // TODO Check correspondence of java and ImageStack index for all for loops where
-    // a ImageStack is accessed
-
-    public RealMatrix padSymetricMatrix(RealMatrix inMatrix, Boolean transMatr){
-        RealMatrix padedMatrix          = null;
-        RealMatrix templateMatrix       = null;
-
-        //transpose if necessary
-        if(transMatr) {
-            templateMatrix = inMatrix.copy().transpose();
-        }else{
-            templateMatrix = inMatrix.copy();
-        }
-
-        padedMatrix          = MatrixUtils.createRealMatrix(templateMatrix.getRowDimension()*2,
-                templateMatrix.getColumnDimension());
-
-        int iPadStart,iPadEnd = 0;
-
-        // symetrical padding
-        for(int i=0;i<=padedMatrix.getRowDimension();i++){
-            iPadStart   = i*2;
-            iPadEnd     = iPadStart+2;
-
-            for(int j=iPadStart;j<=iPadEnd;j++){
-                padedMatrix.setRowVector(j,templateMatrix.getRowVector(i));
-            }
-        }
-
-        return(padedMatrix);
-    }
-
-    public RealMatrix stack2matrix(ImageStack imStack){
-
-        // TODO Finish development if necessary
-
-        RealMatrix stack2matrix = MatrixUtils.createRealMatrix(
-                imStack.getProcessor(0).getHeight()*imStack.getSize(),
-                imStack.getProcessor(0).getWidth()
-        );
-
-        int iPadStart,iPadEnd = 0;
-
-        for(int i=0;i<imStack.getSize();i++){
-            iPadStart   = i*imStack.getProcessor(0).getHeight();
-            iPadEnd     = iPadStart+imStack.getProcessor(0).getHeight();
-
-            /**double[][] stackSlice = imStack.getProcessor(i).convertToFloatProcessor().getBufferedImage();
-            int iSlice = 0 ;
-            double[] vec = stackSlice[];
-
-            for(int j=iPadStart;j<=iPadEnd;j++){
-                stack2matrix.setRowVector(j,);
-            }
-             **/
-        }
-
-        return(stack2matrix);
-    }
-
-    public ImageStack find_base(RealMatrix inMatrix, int indexK){
-        ImageStack baseMatrix           = null;
-
-        int nrowsMat = inMatrix.getRowDimension();
-        int ncolsMat = inMatrix.getColumnDimension();
-
-        int loop    =   0;
-        int sz1     =   inMatrix.getRowDimension()-indexK+1;
-        int sz2     =   inMatrix.getColumnDimension()-indexK+1;
-
-        for(int inx=0;inx<indexK;inx++){
-            for(int iny=0;inx<indexK;inx++){
-                RealMatrix subMatrix = inMatrix.getSubMatrix(inx,(inx+sz1-1),iny,(iny+sz2-1)).copy();
-
-                baseMatrix.addSlice((ImageProcessor) new FloatProcessor(
-                                SME_ENS_Utils.convertDoubleMatrixToFloat(
-                                        subMatrix.getData(),nrowsMat,ncolsMat
-                                )));
-                loop= loop+1;
-            }
-        }
-
-        int indxRetour      = (int) Math.pow(indexK,2);
-        baseMatrix.deleteSlice(indxRetour);
-        /**
-         base(:,:,ceil((k^2)/2))=[];
-         */
-
-        return(baseMatrix);
-    }
 }
--- IJ2/ij/plugin/filter/SME_PROJECTION_SRC/SME_ENS_Utils.java
+++ IJ2/ij/plugin/filter/SME_PROJECTION_SRC/SME_ENS_Utils.java
@@ -1,5 +1,15 @@
 package ij.plugin.filter.SME_PROJECTION_SRC;
 
+import ij.ImageStack;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+import org.apache.commons.math3.exception.MathArithmeticException;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
+
 /**
  * Created by eltonr on 19/03/16.
  */
@@ -67,5 +77,233 @@ public final class SME_ENS_Utils {
         }
 
         return output;
+    }
+
+    public static RealMatrix stack2matrix(ImageStack imStack){
+
+        // TODO Finish development if necessary
+
+        RealMatrix stack2matrix = MatrixUtils.createRealMatrix(
+                imStack.getProcessor(0).getHeight()*imStack.getSize(),
+                imStack.getProcessor(0).getWidth()
+        );
+
+        int iPadStart,iPadEnd = 0;
+
+        for(int i=0;i<imStack.getSize();i++){
+            iPadStart   = i*imStack.getProcessor(0).getHeight();
+            iPadEnd     = iPadStart+imStack.getProcessor(0).getHeight();
+
+            /**double[][] stackSlice = imStack.getProcessor(i).convertToFloatProcessor().getBufferedImage();
+             int iSlice = 0 ;
+             double[] vec = stackSlice[];
+
+             for(int j=iPadStart;j<=iPadEnd;j++){
+             stack2matrix.setRowVector(j,);
+             }
+             **/
+        }
+
+        return(stack2matrix);
+    }
+
+    // TODO Check correspondence of java and ImageStack index for all for loops where
+    // a ImageStack is accessed
+
+    public static RealMatrix padSymetricMatrix(RealMatrix inMatrix, Boolean transMatr){
+        RealMatrix padedMatrix          = null;
+        RealMatrix templateMatrix       = null;
+
+        //transpose if necessary
+        if(transMatr) {
+            templateMatrix = inMatrix.copy().transpose();
+        }else{
+            templateMatrix = inMatrix.copy();
+        }
+
+        padedMatrix          = MatrixUtils.createRealMatrix(templateMatrix.getRowDimension()*2,
+                templateMatrix.getColumnDimension());
+
+        int iPadStart,iPadEnd = 0;
+
+        // symetrical padding
+        for(int i=0;i<=padedMatrix.getRowDimension();i++){
+            iPadStart   = i*2;
+            iPadEnd     = iPadStart+2;
+
+            for(int j=iPadStart;j<=iPadEnd;j++){
+                padedMatrix.setRowVector(j,templateMatrix.getRowVector(i));
+            }
+        }
+
+        return(padedMatrix);
+    }
+
+
+    public static ImageStack find_base(RealMatrix inMatrix, int indexK){
+        ImageStack baseMatrix           = new ImageStack(inMatrix.getRowDimension(),
+                inMatrix.getColumnDimension());
+
+        int nrowsMat = inMatrix.getRowDimension();
+        int ncolsMat = inMatrix.getColumnDimension();
+
+        int loop    =   0;
+        int sz1     =   inMatrix.getRowDimension()-indexK+1;
+        int sz2     =   inMatrix.getColumnDimension()-indexK+1;
+
+        for(int inx=0;inx<indexK;inx++){
+            for(int iny=0;inx<indexK;inx++){
+                RealMatrix subMatrix = inMatrix.getSubMatrix(inx,(inx+sz1-1),iny,(iny+sz2-1)).copy();
+
+                baseMatrix.addSlice((ImageProcessor) new FloatProcessor(
+                        SME_ENS_Utils.convertDoubleMatrixToFloat(
+                                subMatrix.getData(),nrowsMat,ncolsMat
+                        )));
+                loop= loop+1;
+            }
+        }
+
+        int indxRetour      = (int) Math.pow(indexK,2);
+        baseMatrix.deleteSlice(indxRetour);
+        /**
+         base(:,:,ceil((k^2)/2))=[];
+         */
+
+        return(baseMatrix);
+    }
+
+    public static ImageStack repmatMatrixVar(RealMatrix inMatrix, ImageStack base){
+        //varold2=sum((base-repmat(Mold,[1 1 8])).^2,3);
+        final int DIMZ = 8;
+
+        ImageStack baseMatrix           = new ImageStack(inMatrix.getRowDimension(),
+                inMatrix.getColumnDimension());
+
+        int nrowsMat                = inMatrix.getRowDimension();
+        int ncolsMat                = inMatrix.getColumnDimension();
+
+        for(int inz=0;inz<DIMZ;inz++){
+                RealMatrix currentSlice = MatrixUtils.createRealMatrix(
+                        convertFloatMatrixToDoubles(base.getProcessor(inz+1).getFloatArray(),
+                        nrowsMat,ncolsMat));
+                RealMatrix tmpSlice     = currentSlice.subtract(inMatrix);
+                tmpSlice     = tmpSlice.power(2);
+                baseMatrix.addSlice((ImageProcessor) new FloatProcessor(
+                        SME_ENS_Utils.convertDoubleMatrixToFloat(
+                                tmpSlice.getData(),nrowsMat,ncolsMat
+                        )));
+        }
+
+        return(baseMatrix);
+    }
+
+    public static RealMatrix realmatrixDoublepow(RealMatrix inMatrix, double expPow){
+        int nrows = inMatrix.getRowDimension();
+        int ncols = inMatrix.getColumnDimension();
+
+        RealMatrix retuRealMatrix = MatrixUtils.createRealMatrix(nrows,ncols);
+
+        for(int i=0;i<nrows;i++){
+            for(int j=0;j<ncols;j++){
+                retuRealMatrix.setEntry(i,j, Math.pow(inMatrix.getEntry(i,j),expPow));
+            }
+        }
+
+        return(retuRealMatrix);
+    }
+
+    public static ImageStack realmatrixCat(ImageStack imageStack, RealMatrix inMatrix){
+
+        ImageStack returStack = imageStack;
+
+        int ncols = inMatrix.getColumnDimension();
+        int nrows = inMatrix.getRowDimension();
+
+        returStack.addSlice( (ImageProcessor) new FloatProcessor(
+                convertDoubleMatrixToFloat(inMatrix.getData(), nrows,ncols))) ;
+
+        return(returStack);
+    }
+
+    public static RealMatrix getMaxProjectionIndex(ImageStack imageStack){
+
+        double[][] maxIndex = new double[imageStack.getProcessor(1).getWidth()]
+                [imageStack.getProcessor(1).getHeight()];
+
+        for(int pixIndexI=0;pixIndexI<=imageStack.getProcessor(1).getWidth();pixIndexI++){
+            for(int pixIndexJ=0;pixIndexJ<=imageStack.getProcessor(1).getWidth();pixIndexJ++){
+                double maxZval = Double.MIN_VALUE;
+
+                for(int pixIndexZ=0;pixIndexZ<=imageStack.getSize();pixIndexZ++){
+                    if(imageStack.getVoxel(pixIndexI,pixIndexJ,pixIndexZ)>maxZval){
+                        maxIndex[pixIndexI][pixIndexJ] = pixIndexZ;
+                        maxZval = imageStack.getVoxel(pixIndexI,pixIndexJ,pixIndexZ);
+                    }
+                }
+            }
+        }
+
+        return(MatrixUtils.createRealMatrix(maxIndex));
+    }
+
+    public static RealMatrix getMinProjectionIndex(ImageStack imageStack){
+
+        double[][] maxIndex = new double[imageStack.getProcessor(1).getWidth()]
+                [imageStack.getProcessor(1).getHeight()];
+
+        for(int pixIndexI=0;pixIndexI<=imageStack.getProcessor(1).getWidth();pixIndexI++){
+            for(int pixIndexJ=0;pixIndexJ<=imageStack.getProcessor(1).getWidth();pixIndexJ++){
+                double minZval = Double.MAX_VALUE;
+
+                for(int pixIndexZ=0;pixIndexZ<=imageStack.getSize();pixIndexZ++){
+                    if(imageStack.getVoxel(pixIndexI,pixIndexJ,pixIndexZ)<minZval){
+                        maxIndex[pixIndexI][pixIndexJ] = pixIndexZ;
+                        minZval = imageStack.getVoxel(pixIndexI,pixIndexJ,pixIndexZ);
+                    }
+                }
+            }
+        }
+
+        return(MatrixUtils.createRealMatrix(maxIndex));
+    }
+
+    public static void replaceRealmatElements(RealMatrix inMatrix,double elVal, double repVal){
+
+        int ncols = inMatrix.getColumnDimension();
+        int nrows = inMatrix.getRowDimension();
+
+        for(int i=0;i<nrows;i++){
+            for(int j=0;j<ncols;j++){
+                if(inMatrix.getEntry(i,j)==elVal){
+                    inMatrix.setEntry(i,j,repVal);
+                }
+            }
+        }
+    }
+
+    public static RealVector realmat2vector(RealMatrix inMatrix, int dimWalk){
+        RealVector retVector = null;
+
+        int ncols = inMatrix.getColumnDimension();
+        int nrows = inMatrix.getRowDimension();
+
+        switch (dimWalk){
+            case 1:
+            {
+                retVector = MatrixUtils.createRealVector(inMatrix.getRow(0));
+                for(int i=1;i<nrows;i++){
+                    retVector.append(inMatrix.getRowVector(i));
+                }
+            }
+            default:
+            {
+                retVector = MatrixUtils.createRealVector(inMatrix.getColumn(0));
+                for(int i=1;i<ncols;i++){
+                    retVector.append(inMatrix.getColumnVector(i));
+                }
+            }
+        }
+
+        return(retVector);
     }
 }
